782c38f631ae81efd6b6c9586277746334c7a734
stephenchou <sccusr@gmail.com>
2020-07-06 18:47:49 -0400

    WIP added project

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const button = document.getElementById("button");
 const body = document.getElementsByTagName('body')
 const nav_links = document.querySelectorAll('#nav_links li')
 
+console.log("hello");
 
 button.addEventListener('click', (e)=>{body[0].style.backgroundColor = "black";})
